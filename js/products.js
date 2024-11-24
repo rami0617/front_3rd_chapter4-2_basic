99b1416b2e64ba24f3e66b1c123fecbb92e9fda7
최아람 <aarmakrr@hanmail.net>
2024-11-24 17:27:45 +0900

    [refactor]: 이미지 width, height 설정

--- js/products.js
+++ js/products.js
@@ -23,6 +23,7 @@ function displayProducts(products) {
         img.src = product.image;
         img.alt = `product: ${product.title}`;
         img.width=250;
+        img.height = 70;
         pictureDiv.appendChild(img);
 
         // Create the product info div
